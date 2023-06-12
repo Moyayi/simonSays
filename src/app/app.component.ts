3aa41ead82c923335412b6ff738cb1ac43f2b722
Pedro <pedro.moyano.garcia@gmail.com>
2023-06-12 17:32:33 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simonSays';
+  simon : number[] = [];
+  player : number [] = [];
+  juego : boolean = false;
+
+  checkOrder(num : number){
+
+
+  }
+
+
+  simonSays(){
+    this.simon.push(Math.round(Math.random()*3))
+    for(let i = 0 ; i < this.simon.length ; i++){
+      let color = document.getElementById(this.simon[i].toString())
+      setTimeout( () => {
+        
+
+      }, 1000)
+    }
+  }
+
+  empezarJuego(){
+    this.juego = true
+    
+  }
+  
+  
 }
