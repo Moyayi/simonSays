4c82b3485beba1bde9c0b6ecf4b8234133f24df0
Pedro <pedro.moyano.garcia@gmail.com>
2023-06-12 22:57:10 +0200

    Added sound when simon says the color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,16 @@ export class AppComponent implements OnInit{
   simon : number[] = [];
   player : number [] = [];
   juego : boolean = false;
-  
+
+  delay : Function = (ms : number) : Promise<void> => {
+    return  new Promise( res => setTimeout(res,ms))
+  }
 
   colores : Colores[] = [
-    {key : 0, clase : "tlSimon"},
-    {key : 1, clase : "trSimon"},
-    {key : 2, clase : "blSimon"},
-    {key : 3, clase : "brSimon"},
+    {key : 0, clase : "tlSimon", sonido : "../assets/music/verde.wav"},
+    {key : 1, clase : "trSimon", sonido : "../assets/music/rojo.wav"},
+    {key : 2, clase : "blSimon", sonido : "../assets/music/amarillo.wav"},
+    {key : 3, clase : "brSimon", sonido : "../assets/music/azul.wav"},
   ]
 
   ngOnInit(): void {
@@ -25,17 +28,19 @@ export class AppComponent implements OnInit{
 
   }
 
-  checkOrder(num : number) : void {
-    //TODO comprobar el orden de el array del jugador con el de simon, for itinerado de jugador comprobar 
+  async checkOrder(num : number) {
+    if( !this.juego ) return 
+
     this.player.push(num)
     for(let i = 0 ; i < this.player.length; i++){
       if( this.player[i] !== this.simon[i]) {
         this.defeated()
-        return
+        return 
       }
     }
     if( this.player.length === this.simon.length){
       this.player = []
+      await this.delay(1000)
       this.simonSays()
     }
 
@@ -43,19 +48,22 @@ export class AppComponent implements OnInit{
 
 
   async simonSays(){
-    debugger
     this.simon.push(Math.round(Math.random()*3))
-    const delay =  (ms : number) =>  new Promise(res => setTimeout(res, ms));
+
     for(let i = 0 ; i < this.simon.length ; i++){
       let color = document.getElementById(this.simon[i].toString())
 
       color?.classList.add(this.colores[this.simon[i]].clase)
+      let audio = new Audio()
+      audio.src = this.colores[this.simon[i]].sonido;
+      audio.load();
+      audio.play();
       
-      await delay(500)
+      await this.delay(550)
       
       color?.classList.remove(this.colores[this.simon[i]].clase)
 
-      await delay(1000)
+      await this.delay(500)
     }
   }
 
--- src/app/interfaces/colores-interface.ts
+++ src/app/interfaces/colores-interface.ts
@@ -3,5 +3,6 @@
 export interface Colores{
     key   : number,
     clase : string,
+    sonido : string
     
 }
